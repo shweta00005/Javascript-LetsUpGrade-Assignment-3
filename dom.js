ddee1c57b23281188494a8abfd071940d2fd826e
shweta00005 <88192265+shweta00005@users.noreply.github.com>
2021-08-05 15:43:32 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,14 @@
+// functions
+
+function doSomething(){
+
+    console.log(document.querySelector("#name").type);
+
+    document.querySelector("#sec").innerText="Shweta";
+
+    console.log(document.querySelector("#city").type);
+
+    document.querySelector("#sec").innerText="Noida";
+
+
+}
